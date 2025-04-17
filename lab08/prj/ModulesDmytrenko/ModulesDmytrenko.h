712a8ddc50bbf38dafeaa794b2eb17a106ae6826
Yaroslava Dmytrenko <dmytrenkovika@gmail.com>
2025-04-17 11:03:46 +0300

    Adding ModulesDmytrenko

--- lab08/prj/ModulesDmytrenko/ModulesDmytrenko.h
+++ lab08/prj/ModulesDmytrenko/ModulesDmytrenko.h
@@ -3,4 +3,29 @@
 
 double s_calculation(double x, double y, double z);
 
+int calculate_bonuses(float sum_purchase);
+
+float purchase_to_pay(float sum_purchase, int bonus);
+
+float degrees(float fahr_degree);
+
+int count_bits(unsigned int N);
+
+void writeAuthorInfo(const std::string& filename);
+bool isCyrillic(char c);
+
+std::string detectLanguage(const std::string& text);
+
+std::string toUpperCase(const std::string& str);
+
+std::string removeForbiddenWords(const std::string& text, const std::vector<std::string>& forbidden);
+
+void appendEducationArticleToInput(const std::string& filename);
+
+std::string educationArtecl();
+
+void appendCurrentDateTime(const std::string& filename);
+
+std::string toBinary(unsigned int b);
+
 #endif // MODULESDMYTRENKO_H_INCLUDED
